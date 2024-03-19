03a326df5111036daffbfb3303dcb88553d1586c
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-19 13:58:07 +0100

    propal structure

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 15:31:14 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/03/19 12:37:42 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/03/19 13:52:55 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,3 +61,25 @@ typedef struct s_operator
 */
 #endif
 
+/*
+typedef	struct s_cmd
+{
+	t_op			op;
+	char			*path;
+	char			**arg;
+	char			**env;
+}	t_cmd;
+
+typedef struct s_operator
+{
+	t_token	token;
+	char	*file;
+}	t_op;
+
+typedef struct s_pipe
+{
+	t_token	token;
+	t_cmd	cmd1;
+	t_cmd	cmd2;
+}	t_pipe;
+*/
