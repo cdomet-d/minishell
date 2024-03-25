da895d0dca395963158f9214bab6e3672b1f5b7f
cdomet-d <coralie.dometdemont@gmail.com>
2024-03-25 18:17:27 +0100

    added exec functions checking for redirections and exec.h for exec specific functions & structures

---
+++ local_functions.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   local_functions.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 12:52:51 by cdomet-d          #+#    #+#             */
+/*   Updated: 2024/03/25 15:51:00 by cdomet-d         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	init_env(char **envp, t_env **env)
+{
+	size_t	i;
+	t_env	*envnew;
+
+	i = 0;
+	while (envp[i])
+	{
+		envnew = env_newnode(envp[i]);
+		if (!envnew)
+			free_env(*env, errno, "Failure while initializing environnement");
+		env_addback(env, envnew);
+		i++;
+	}
+}
+
+void	init_input(char *argv[], t_input **input, t_env *env)
+{
+	size_t	i;
+	t_input	*inew;
+
+	i = 1;
+	while (argv[i])
+	{
+		inew = input_newnode(ft_split(argv[i], ' '), token(argv[i]), env);
+		if (!inew)
+			free_all(input, errno, "Failure while initializing input");
+		input_addback(input, inew);
+		i++;
+	}
+}
+
+int	token(char *str)
+{
+	if (str[0] == '|')
+		return (pip);
+	else if (str[0] == '<')
+		return (in_bracket);
+	else if (str[0] == '>')
+		return (out_bracket);
+	else
+		return (command);
+}
