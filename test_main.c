699a128ccc812202740f17e73deb1fb12eb05282
cdomet-d <coralie.dometdemont@gmail.com>
2024-03-25 18:17:54 +0100

    added exec functions checking for redirections and exec.h for exec specific functions & structures

--- test_main.c
+++ test_main.c
@@ -6,7 +6,7 @@
 /*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 11:50:37 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/03/25 18:12:56 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/03/25 18:17:48 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	main(int argc, char *argv[], char *envp[])
 		return (EXIT_FAILURE);
 	init_env(envp, &env);
 	init_input(argv, &input, env);
-	count_pipes(input)
+	count_pipes(input, NULL);
 	print_in_for(input);
 	free_all(&input, EXIT_SUCCESS, NULL);
 }
