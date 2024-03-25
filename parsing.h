6fa45d4685dc0c0a02aef90888aee16168b6d33a
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-25 19:34:17 +0100

    corrected spelling mistake

--- parsing.h
+++ parsing.h
@@ -4,6 +4,6 @@
 # define PARSING_H
 
 void    create_env(char **envp, t_env **env);
-void    tokenizaton(t_input **input, t_env **env);
+void    tokenization(t_input **input, t_env **env, char *line);
 
 #endif
