05779b2aa79cd2cc74f3a6058dacd9f92c956d97
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-25 19:31:57 +0100

    added while with readline to get input

--- main.c
+++ main.c
@@ -5,12 +5,31 @@
 
 int main(int argc, char **argv, char **envp)
 {
-    t_env   *env;
-    t_input *input;
+	t_env   *env;
+	t_input *input;
+	char    *line;
 
-    (void)argc;
-    (void)argv;
-    create_env(envp, &env);
-    tokenization(&input, &env);
-    return (0);
+	(void)argc;
+	(void)argv;
+	line = NULL;
+	env = NULL;
+	input = NULL;
+	create_env(envp, &env);
+	while (1)
+	{
+		line = readline("Minishell > ");
+		if (ft_strncmp(line, "exit", ft_strlen(line)) == 0) //si line == NULL ca s'arrete
+			break ;
+		tokenization(&input, &env, line);
+	}
+	while (input)
+	{
+		printf("input->tok: %d\n", input->tok);
+		if (input->data)
+			printf("input->data: %s\n", input->data[0]);
+		input = input->next;
+	}
+	free_env(env, EXIT_SUCCESS, NULL);
+	free_all(&input, EXIT_SUCCESS, NULL);
+	return (0);
 }
