e2f652563d02f631abece969c3ac665b3e42573b
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-25 18:16:17 +0100

    added protection to create_env

--- create_env_lst.c
+++ create_env_lst.c
@@ -8,9 +8,13 @@ void    create_env(char **envp, t_env **env)
     int     i;
     t_env   *new;
 
+    i = 0;
+    new = NULL;
     while (envp[i])
     {
         new = env_newnode(envp[i]);
+        if (!new)
+            free_env(*env, EXIT_FAILURE, NULL);
         env_addback(env, new);
         i++;
     }
