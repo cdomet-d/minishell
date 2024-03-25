edb4ec9ac709136290fa65fb1f6dcff912855049
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-25 19:35:22 +0100

    add build tab to create var data for input struct

--- tokenization.c
+++ tokenization.c
@@ -6,35 +6,74 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 15:19:47 by csweetin          #+#    #+#             */
-/*   Updated: 2024/03/25 18:00:29 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/03/25 19:27:58 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "parsing.h"
 
-void	create_input(t_input **input, t_env **env, char *str, int tok)
+void	create_input(t_input **input, t_env **env, char **data, int tok)
 {
-    t_input	*new;
-	char	**data;
-	// int		size;
+	t_input	*new;
 
-	(void)str;
-    new = NULL;
-	data = NULL;
-	// size = 0;
-	// if (tok == command || tok == file || tok == delimiter)
-	// {
-		
-	// }
-    new = input_newnode(data, tok, *env);
+	new = NULL;
+	new = input_newnode(data, tok, *env);
 	if (!new)
 		free_all(input, errno, NULL);
 	input_addback(input, new);
 }
 
+char	**build_tab(char *line, int *i, char sep)
+{
+	char	**tab;
+	int		word;
+	int		letter;
+	int		temp;
+
+	tab = NULL;
+	word = 0;
+	letter = 0;
+	if (sep == '"' || sep == '\'')
+	{
+		letter = 2;
+		word = 1;
+		*i += 1;
+	}
+	tab = malloc(sizeof(char *) * (word + 1));
+	if (!tab)
+		return (NULL);
+	temp = *i;
+	while (line[temp] != sep)
+	{
+		letter++;
+		temp++;
+	}
+	temp = *i - 1;
+	word = 0;
+	tab[word] = malloc(sizeof(char) * (letter + 1));
+	if (!tab[word])
+	{
+		free_dtab(tab);
+		return (NULL);
+	}
+	letter = 0;
+	if (sep == '"' || sep == '\'')
+		tab[word][letter++] = line[temp++];
+	while (line[temp] != sep)
+		tab[word][letter++] = line[temp++];
+	if (sep == '"' || sep == '\'')
+		tab[word][letter++] = line[temp++];
+	tab[word][letter++] = '\0';
+	tab[word + 1] = NULL;
+	return (tab);
+}
+
 void	tok_inredir(t_input **input, t_env **env, char *line, int *i)
 {
+	char	**data;
+
+	data = NULL;
 	if (line[*i + 1] == '<')
 	{
 		create_input(input, env, NULL, heredoc);
@@ -43,10 +82,23 @@ void	tok_inredir(t_input **input, t_env **env, char *line, int *i)
 	else
 	{
 		create_input(input, env, NULL, inredir);
-		if (line[*i + 1] == '"')
-			build_tab();
-		while (line[*i + 1] != '<' || line[*i + 1] != '<' || line[*i + 1] != '<')
-			;
+		*i += 1;
+		while (line[*i] >= '\t' && line[*i] <= '\r')
+		{
+			printf("in\n");
+			*i += 1;
+		}
+		printf("%d\n", *i);
+		if (line[*i] == '"')
+		{
+			data = build_tab(line, i, '"');
+			if (!data)
+				free_all(input, errno, NULL);
+			create_input(input, env, data, file);
+		}
+		// while (line[*i + 1] != '<' || line[*i + 1] != '>'
+		// 	|| line[*i + 1] != '|')
+		// 	;
 	}
 }
 
