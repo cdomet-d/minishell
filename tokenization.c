6045e24b77c41ec187f469cbbe1df6c346611a29
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-26 16:04:09 +0100

    ras

--- tokenization.c
+++ tokenization.c
@@ -6,83 +6,11 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 15:19:47 by csweetin          #+#    #+#             */
-/*   Updated: 2024/03/25 19:27:58 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/03/26 15:50:40 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
 #include "parsing.h"
-
-void	create_input(t_input **input, t_env **env, char **data, int tok)
-{
-	t_input	*new;
-
-	new = NULL;
-	new = input_newnode(data, tok, *env);
-	if (!new)
-		free_all(input, errno, NULL);
-	input_addback(input, new);
-}
-
-char	**build_tab(char *line, int *i, int word)
-{
-	char	**tab;
-	int		letter;
-	int		temp;
-	int		quote;
-
-	tab = NULL;
-	quote = 0;
-	// word = 0;
-	letter = 0;
-	tab = malloc(sizeof(char *) * (word + 1));
-	if (!tab)
-		return (NULL);
-	temp = *i;
-	while (line[temp] != ' ' && line[temp] != '>' && line[temp] != '<' && line[temp] != '|')
-	{
-		letter++;
-		temp++;
-	}
-	word = 0;
-	tab[word] = malloc(sizeof(char) * (letter + 1));
-	if (!tab[word])
-	{
-		free_dtab(tab);
-		return (NULL);
-	}
-	letter = 0;
-	while (line[*i] && line[*i] != ' ' && line[*i] != '>' && line[*i] != '<' && line[*i] != '|')
-	{
-		if (line[*i] == '"')
-		{
-			quote++;
-			tab[word][letter++] = line[*i];
-			*i += 1;
-			while (line[*i] && line[*i] != '"')
-			{
-				tab[word][letter++] = line[*i];
-				*i += 1;
-			}
-			if (line[*i] == '"')
-				quote++;
-		}
-		if (line[*i])
-		{
-			tab[word][letter++] = line[*i];
-			*i += 1;
-		}
-	}
-	tab[word][letter] = '\0';
-	tab[word + 1] = NULL;
-	if ((quote % 2) != 0)
-	{
-		write(2, "syntax error\n", 13);
-		free_dtab(tab);
-		tab = NULL;
-	}
-	return (tab);
-}
 
 void	tok_inredir(t_input **input, t_env **env, char *line, int *i)
 {
@@ -96,7 +24,6 @@ void	tok_inredir(t_input **input, t_env **env, char *line, int *i)
 	}
 	else
 	{
-		create_input(input, env, NULL, inredir);
 		*i += 1;
 		while ((line[*i] >= '\t' && line[*i] <= '\r') || line[*i] == ' ')
 			*i += 1;
@@ -106,7 +33,7 @@ void	tok_inredir(t_input **input, t_env **env, char *line, int *i)
 			if (!data)
 				free_all(input, errno, NULL);
 			if (data)
-				create_input(input, env, data, file);
+				create_input(input, env, data, inredir);
 		}
 	}
 }
