5a6a713ce1b4ca51f49e4a5449418cbff07d6561
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-26 17:39:32 +0100

    split build_tab funct

--- create_input_lst.c
+++ create_input_lst.c
@@ -6,7 +6,7 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 15:40:32 by csweetin          #+#    #+#             */
-/*   Updated: 2024/03/26 15:44:17 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/03/26 17:37:34 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,39 +23,47 @@ void	create_input(t_input **input, t_env **env, char **data, int tok)
 	input_addback(input, new);
 }
 
-char	**build_tab(char *line, int *i, int word)
+int	count_letter(char *line, int count)
 {
-	char	**tab;
-	int		letter;
 	int		temp;
 	int		quote;
+	char	quotetype;
 
-	tab = NULL;
 	quote = 0;
-	// word = 0;
-	letter = 0;
-	tab = malloc(sizeof(char *) * (word + 1));
-	if (!tab)
-		return (NULL);
-	temp = *i;
-	while (line[temp] != ' ' && line[temp] != '>' && line[temp] != '<' && line[temp] != '|')
+	temp = count;
+	while (line[count] && line[count] != ' ' && line[count] != '>'
+		&& line[count] != '<' && line[count] != '|')
 	{
-		letter++;
-		temp++;
+		if (line[count] == '"' || line[count] == '\'')
+		{
+			quotetype = line[count];
+			quote++;
+			while (line[count] && line[count] != '"')
+				count++;
+			if (line[count] == quotetype)
+				quote++;
+		}
+		if (line[count])
+			count++;
 	}
+	if ((quote % 2) != 0)
+		return (-1);
+	count -= temp;
+	return (count);
+}
+
+void	fill_tab(char *line, int *i, char **tab)
+{
+	int	word;
+	int	letter;
+
 	word = 0;
-	tab[word] = malloc(sizeof(char) * (letter + 1));
-	if (!tab[word])
-	{
-		free_dtab(tab);
-		return (NULL);
-	}
 	letter = 0;
-	while (line[*i] && line[*i] != ' ' && line[*i] != '>' && line[*i] != '<' && line[*i] != '|')
+	while (line[*i] && line[*i] != ' ' && line[*i] != '>'
+		&& line[*i] != '<' && line[*i] != '|')
 	{
 		if (line[*i] == '"')
 		{
-			quote++;
 			tab[word][letter++] = line[*i];
 			*i += 1;
 			while (line[*i] && line[*i] != '"')
@@ -63,8 +71,6 @@ char	**build_tab(char *line, int *i, int word)
 				tab[word][letter++] = line[*i];
 				*i += 1;
 			}
-			if (line[*i] == '"')
-				quote++;
 		}
 		if (line[*i])
 		{
@@ -74,11 +80,24 @@ char	**build_tab(char *line, int *i, int word)
 	}
 	tab[word][letter] = '\0';
 	tab[word + 1] = NULL;
-	if ((quote % 2) != 0)
-	{
-		write(2, "syntax error\n", 13);
-		free_dtab(tab);
-		tab = NULL;
-	}
+}
+
+char	**build_tab(char *line, int *i, int word)
+{
+	char	**tab;
+	int		letter;
+
+	tab = NULL;
+	letter = 0;
+	if (word != 1)
+		word = count_word();
+	tab = malloc(sizeof(char *) * (word + 1));
+	if (!tab)
+		return (NULL);
+	letter = count_letter(line, *i);
+	tab[word] = malloc(sizeof(char) * (letter + 1));
+	if (!tab[word])
+		return (free_dtab(tab), NULL);
+	fill_tab(line, i, tab);
 	return (tab);
 }
