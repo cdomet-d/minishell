cb0023d2db7035a6f627eaa010241e6f0c3c50ae
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-28 17:48:01 +0100

    comment all

--- test_main.c
+++ test_main.c
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   test_main.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 11:50:37 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/03/26 16:20:06 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/03/28 16:29:29 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-int	main(int argc, char *argv[], char *envp[])
-{
-	t_env	*env;
-	t_input	*input;
+// int	main(int argc, char *argv[], char *envp[])
+// {
+// 	t_env	*env;
+// 	t_input	*input;
 
-	env = NULL;
-	input = NULL;
-	if (argc < 2)
-		return (EXIT_FAILURE);
-	init_env(envp, &env);
-	init_input(argv, &input, env);
-	exec_cmd(input);
-	fatal_exit(&input, EXIT_SUCCESS, NULL);
-}
+// 	env = NULL;
+// 	input = NULL;
+// 	if (argc < 2)
+// 		return (EXIT_FAILURE);
+// 	init_env(envp, &env);
+// 	init_input(argv, &input, env);
+// 	exec_cmd(input);
+// 	fatal_exit(&input, EXIT_SUCCESS, NULL);
+// }
