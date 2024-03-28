0ea2bb3eb84b3761de43b9f323644144c4cff8b4
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-28 17:47:49 +0100

    changed protype of create_env

--- parsing.h
+++ parsing.h
@@ -6,7 +6,7 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 15:41:26 by csweetin          #+#    #+#             */
-/*   Updated: 2024/03/27 18:45:59 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/03/28 17:38:03 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "minishell.h"
 
-void	create_env(char **envp, t_env **env);
+void	create_env(t_input **input, char **envp, t_env **env);
 void	tokenization(t_input **input, t_env **env, char *line);
 void	create_input(t_input **input, t_env **env, char **data, int tok);
 char	**build_tab(char *line, int *i, int word);
