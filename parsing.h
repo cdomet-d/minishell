15e0260dd8e7db432927edb4862a94e2c90e3bb6
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-28 18:05:11 +0100

    added prototype parsing

--- parsing.h
+++ parsing.h
@@ -6,7 +6,7 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 15:41:26 by csweetin          #+#    #+#             */
-/*   Updated: 2024/03/28 17:38:03 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/03/28 18:01:10 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 
 # include "minishell.h"
 
-void	create_env(t_input **input, char **envp, t_env **env);
+void	parsing(t_input **input, t_env **env, char *line);
 void	tokenization(t_input **input, t_env **env, char *line);
+void	create_env(t_input **input, char **envp, t_env **env);
 void	create_input(t_input **input, t_env **env, char **data, int tok);
 char	**build_tab(char *line, int *i, int word);
 int		check_quote(char *line);
