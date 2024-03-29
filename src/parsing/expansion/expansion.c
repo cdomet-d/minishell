78b1e34b0b2062b29f1e46663e4ad834bbb17bf2
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-29 19:09:25 +0100

    added functions command_expand && op_expand

--- src/parsing/expansion/expansion.c
+++ src/parsing/expansion/expansion.c
@@ -6,22 +6,37 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 18:28:27 by csweetin          #+#    #+#             */
-/*   Updated: 2024/03/29 17:49:24 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/03/29 18:53:59 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-//si $arg=NULL replace par rien
+//si $arg=NULL ou n'existe pas replace par rien
+
+void	command_expand()
+{
+
+}
+
+// void	op_expand(char *data, t_env **env)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	if (data[i++] == '\'')
+// 	{
+// 		while (data[i] && data[i] != '\'')
+// 			i++;
+		
+// 	}
+// }
 
 void	expand(t_input *node, t_env **env)
 {
-	int	i;
-
 	(void)env;
-	i = 0;
-	while (node->data)
-	{
-		
-	}
+	if (node->tok == command)
+		command_expand();
+	// else
+	// 	op_expand(node->data[0], env);
 }
