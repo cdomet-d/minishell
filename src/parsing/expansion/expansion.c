3c4f4ebcbe8fc049b923f996a2b81679acb66dc3
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-03-29 19:22:40 +0100

    started coding expand functions

--- src/parsing/expansion/expansion.c
+++ src/parsing/expansion/expansion.c
@@ -6,7 +6,7 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 18:28:27 by csweetin          #+#    #+#             */
-/*   Updated: 2024/03/29 18:53:59 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/03/29 19:22:01 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,45 @@ void	command_expand()
 
 }
 
-// void	op_expand(char *data, t_env **env)
-// {
-// 	int	i;
+char	*search_env(char *data, t_env **env)
+{
+	char	*var;
+	t_env	*node;
 
-// 	i = 0;
-// 	if (data[i++] == '\'')
-// 	{
-// 		while (data[i] && data[i] != '\'')
-// 			i++;
-		
-// 	}
-// }
+	(void)data;
+	var = NULL;
+	node = *env;
+	while (node->env)
+	{
+		node = node->next;
+	}
+	return (var);
+}
+
+void	op_expand(char *data, t_env **env)
+{
+	int	i;
+
+	i = 0;
+	while (data[i])
+	{
+		if (data[i++] == '\'')
+		{
+			while (data[i] && data[i] != '\'')
+				i++;
+		}
+		if (data[i] == '$')
+			search_env(data + i, env);
+		if (data[i])
+			i++;
+	}
+}
 
 void	expand(t_input *node, t_env **env)
 {
 	(void)env;
 	if (node->tok == command)
 		command_expand();
-	// else
-	// 	op_expand(node->data[0], env);
+	else
+		op_expand(node->data[0], env);
 }
