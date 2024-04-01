13b3052948a8f0c1c1e94159abaac38e1b263432
cha202 <116841786+cha202@users.noreply.github.com>
2024-04-01 16:20:08 +0200

    organized header

--- includes/parsing.h
+++ includes/parsing.h
@@ -16,19 +16,36 @@
 # include "minishell.h"
 # include "exec.h"
 
+/*----------------------------- PARSING -----------------------------*/
+
 void	parsing(t_input **input, t_env **env, char *line);
-void	expand(t_input *node, t_env **env);
-void	create_env(t_input **input, char **envp, t_env **env);
-void	count_word(char *line, int i, int *word);
-char	**build_tab(char *line, int *i, int word);
-char	**get_data(t_input **input, char *line, int *i);
-int		create_input(t_input **input, t_env **env, char **data, int tok);
+
+/*----------------------------TOKENIZATION ---------------------------*/
+
 int		tokenization(t_input **input, t_env **env, char *line);
-int		check_quote(char *line);
-int		check_for_dollar(t_input *node);
 int		tok_inredir(t_input **input, t_env **env, char *line, int *i);
 int		tok_outredir(t_input **input, t_env **env, char *line, int *i);
 int		tok_command(t_input **input, t_env **env, char *line, int *i);
 int		tok_pipe(t_input **input, t_env **env, char *line, int *i);
+
+/* tokenization utils */
+int		check_quote(char *line);
+void	count_word(char *line, int i, int *word);
+char	**get_data(t_input **input, char *line, int *i);
+char	**build_tab(char *line, int *i, int word);
+
+/*----------------------------- EXPANSION -----------------------------*/
+
+void	expand(t_input *node, t_env **env);
+
+/* parsing utils */
+int		check_for_dollar(t_input *node);
+
+/*---------------------------- CREATE LISTS ----------------------------*/
+
+void	create_env(t_input **input, char **envp, t_env **env);
+int		create_input(t_input **input, t_env **env, char **data, int tok);
+
+/*----------------------------------------------------------------------*/
 
 #endif
