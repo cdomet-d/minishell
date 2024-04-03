082db5cb25f9d0a309312fd8605b1e972f3d8c9f
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-04-03 16:37:20 +0200

    counting letters

--- src/parsing/expansion/expansion.c
+++ src/parsing/expansion/expansion.c
@@ -6,7 +6,7 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 18:28:27 by csweetin          #+#    #+#             */
-/*   Updated: 2024/04/02 18:51:27 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/04/03 16:35:25 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,66 +63,102 @@ void	nb_word(char **data, t_env **env, int *word)
 	}
 }
 
-void	nb_letter_env(char *str, int *letter)
+void	letter_env_quote(char *str, int *letter)
 {
-	(void)str;
-	(void)letter;
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != '=')
+		i += 1;
+	i += 1;
+	while (str[i])
+	{
+		*letter += 1;
+		i++;
+	}
 }
 
-void	nb_letter(char *str, t_env **env, int *letter)
+void	nb_letter_env(char *str, int *letter, int *i)
 {
-	while (str[*letter])
+	while (str[*i] && str[*i] != '=')
+		*i += 1;
+	*i += 1;
+	printf("str[%d] : %c\n", *i, str[*i]);
+	while (str[*i] && str[*i] != ' ' && (str[*i] < '\t' || str[*i] > '\r'))
 	{
-		if (str[*letter] == '\'')
+		*letter += 1;
+		*i += 1;
+	}
+	printf("letter : %d\n", *letter);
+}
+
+void	nb_letter(char *str, t_env **env, int *letter, int *i)
+{
+	if (str[*i] == '\'')
+	{
+		*letter += 1;
+		*i += 1;
+		while (str[*i] && str[*i] != '\'')
 		{
+			*i += 1;
 			*letter += 1;
-			while (str[*letter] && str[*letter] != '\'')
-				*letter += 1;
 		}
-		if (str[*letter] == '"')
+	}
+	if (str[*i] == '"')
+	{
+		*i += 1;
+		*letter += 1;
+		while (str[*i] && str[*i] != '"')
 		{
+			if (str[*i] == '$')
+				letter_env_quote(search_env(str + (*i + 1), env), letter);
+			*i += 1;
 			*letter += 1;
-			while (str[*letter] && str[*letter] != '"')
-			{
-				if (str[*letter] == '$')
-					nb_letter_env(search_env(str + (*letter + 1), env), letter);
-				*letter += 1;
-			}
 		}
-		if (str[*letter] == '$')
-			nb_letter_env(search_env(str + (*letter + 1), env), letter);
-		if (str[*letter])
-			*letter += 1;
 	}
 }
 
 int	expand(t_input *node, t_env **env)
 {
-	char	**newtab;
-	char	**temp;
+	// char	**newtab;
+	// char	**temp;
 	int		word;
 	int		letter;
+	int		pos;
+	int		i;
 
-	newtab = NULL;
-	temp = NULL;
+	// newtab = NULL;
+	// temp = NULL;
 	word = 0;
+	pos = 0;
 	nb_word(node->data, env, &word);
-	newtab = ft_calloc(sizeof(char *), word + 1);
-	if (!newtab)
-		return (1);
+	// newtab = ft_calloc(sizeof(char *), word + 1);
+	// if (!newtab)
+	// 	return (1);
 	word = 0;
 	while (node->data[word])
 	{
+		i = 0;
 		letter = 0;
-		nb_letter(node->data[word], env, &letter);
-		newtab = ft_calloc(sizeof(char), letter + 1);
-		if (!newtab)
-			return (1);
-		ft_fill();
+		while (node->data[word][i])
+		{
+			nb_letter(node->data[word], env, &letter, &pos);
+			if (node->data[word][i] == '$')
+			{
+				nb_letter_env(search_env(node->data[word] + (i + 1), env), &letter, &pos);
+			}
+			if (node->data[word][i])
+				i += 1;
+		}
+		printf("letter : %d\n", letter);
+		// newtab = ft_calloc(sizeof(char), letter + 1);
+		// if (!newtab)
+		// 	return (1);
+		// ft_fill();
 		word++;
 	}
-	temp = node->data;
-	node->data = NULL;
-	free_dtab(node->data);
+	// temp = node->data;
+	// node->data = NULL;
+	// free_dtab(node->data);
 	return (0);
 }
