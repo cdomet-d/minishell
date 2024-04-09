e0935667e2f7fc5c157a82bea32917eb58d88875
cha202 <116841786+cha202@users.noreply.github.com>
2024-04-09 14:08:43 +0200

    put everything in comments

--- src/parsing/expansion/test/tries/fill.c
+++ src/parsing/expansion/test/tries/fill.c
@@ -1,105 +1,105 @@
-#include "parsing.h"
+// #include "parsing.h"
 
-void	fill_env_quote(char *env, char *new, int *letter)
-{
-	int	i;
+// void	fill_env_quote(char *env, char *new, int *letter)
+// {
+// 	int	i;
 
-	i = 0;
-	if (!env)
-		return ;
-	while (env[i] && env[i] != '=')
-		i++;
-	i++;
-	while (env[i])
-	{
-		new[*letter] = env[i]; 
-		*letter += 1;
-		i++;
-	}
-}
+// 	i = 0;
+// 	if (!env)
+// 		return ;
+// 	while (env[i] && env[i] != '=')
+// 		i++;
+// 	i++;
+// 	while (env[i])
+// 	{
+// 		new[*letter] = env[i]; 
+// 		*letter += 1;
+// 		i++;
+// 	}
+// }
 
-void	fill_env(char *env, char **newtab, int *word, int *letter)
-{
-	int	i;
+// void	fill_env(char *env, char **newtab, int *word, int *letter)
+// {
+// 	int	i;
 
-	i = 0;
-	if (!env)
-		return ;
-	while (env[i] && env[i] != '=')
-		i++;
-	i++;
-	while (env[i])
-	{
-		if (env[i] && (env[i] == ' ' || (env[i] >= '\t' && env[i] <= '\r')))
-		{
-			while (env[i] && (env[i] == ' ' || (env[i] >= '\t' && env[i] <= '\r')))
-				i++;
-			if (env[i])
-			{
-				*letter = 0;
-				*word += 1;
-			}
-		}
-		if (env[i])
-		{
-			newtab[*word][*letter] = env[i];
-			i++;
-			*letter += 1;
-		}
-	}
-}
+// 	i = 0;
+// 	if (!env)
+// 		return ;
+// 	while (env[i] && env[i] != '=')
+// 		i++;
+// 	i++;
+// 	while (env[i])
+// 	{
+// 		if (env[i] && (env[i] == ' ' || (env[i] >= '\t' && env[i] <= '\r')))
+// 		{
+// 			while (env[i] && (env[i] == ' ' || (env[i] >= '\t' && env[i] <= '\r')))
+// 				i++;
+// 			if (env[i])
+// 			{
+// 				*letter = 0;
+// 				*word += 1;
+// 			}
+// 		}
+// 		if (env[i])
+// 		{
+// 			newtab[*word][*letter] = env[i];
+// 			i++;
+// 			*letter += 1;
+// 		}
+// 	}
+// }
 
-void	ft_fill(char **data, t_env **env, char **newtab)
-{
-	int	i;
-	int	j;
-	int	word;
-	int	letter;
+// void	ft_fill(char **data, t_env **env, char **newtab)
+// {
+// 	int	i;
+// 	int	j;
+// 	int	word;
+// 	int	letter;
 
-	i = 0;
-	word = 0;
-	while (data[i])
-	{
-		letter = 0;
-		j = 0;
-		while (data[i][j])
-		{
-			if (data[i][j] == '\'')
-			{
-				newtab[word][letter++] = data[i][j++];
-				while (data[i][j] && data[i][j] != '\'')
-					newtab[word][letter++] = data[i][j++];
-			}
-			if (data[i][j] == '"')
-			{
-				newtab[word][letter++] = data[i][j++];
-				while (data[i][j] && data[i][j] != '"')
-				{
-					if (data[i][j] == '$')
-					{
-						j++;
-						fill_env_quote(search_env(data[i] + j, env), newtab[word], &letter);
-						while (data[i][j] && data[i][j] != '$' && data[i][j] != '"'
-							&& data[i][j] != '\'' && data[i][j] != ' '
-							&& (data[i][j] < '\t' || data[i][j] > '\r'))
-							j++;
-					}
-					else if (data[i][j])
-						newtab[word][letter++] = data[i][j++];
-				}
-			}
-			if (data[i][j] && data[i][j] == '$')
-			{
-				j++;
-				fill_env(search_env(data[i] + j, env), newtab, &word, &letter);
-				while (data[i][j] && data[i][j] != '$' && data[i][j] != '"'
-					&& data[i][j] != '\'')
-					j++;
-			}
-			else if (data[i][j])
-				newtab[word][letter++] = data[i][j++];
-		}
-		i++;
-		word++;
-	}
-}
+// 	i = 0;
+// 	word = 0;
+// 	while (data[i])
+// 	{
+// 		letter = 0;
+// 		j = 0;
+// 		while (data[i][j])
+// 		{
+// 			if (data[i][j] == '\'')
+// 			{
+// 				newtab[word][letter++] = data[i][j++];
+// 				while (data[i][j] && data[i][j] != '\'')
+// 					newtab[word][letter++] = data[i][j++];
+// 			}
+// 			if (data[i][j] == '"')
+// 			{
+// 				newtab[word][letter++] = data[i][j++];
+// 				while (data[i][j] && data[i][j] != '"')
+// 				{
+// 					if (data[i][j] == '$')
+// 					{
+// 						j++;
+// 						fill_env_quote(search_env(data[i] + j, env), newtab[word], &letter);
+// 						while (data[i][j] && data[i][j] != '$' && data[i][j] != '"'
+// 							&& data[i][j] != '\'' && data[i][j] != ' '
+// 							&& (data[i][j] < '\t' || data[i][j] > '\r'))
+// 							j++;
+// 					}
+// 					else if (data[i][j])
+// 						newtab[word][letter++] = data[i][j++];
+// 				}
+// 			}
+// 			if (data[i][j] && data[i][j] == '$')
+// 			{
+// 				j++;
+// 				fill_env(search_env(data[i] + j, env), newtab, &word, &letter);
+// 				while (data[i][j] && data[i][j] != '$' && data[i][j] != '"'
+// 					&& data[i][j] != '\'')
+// 					j++;
+// 			}
+// 			else if (data[i][j])
+// 				newtab[word][letter++] = data[i][j++];
+// 		}
+// 		i++;
+// 		word++;
+// 	}
+// }
--- src/parsing/expansion/test/tries/word.c
+++ src/parsing/expansion/test/tries/word.c
@@ -2,130 +2,130 @@
 
 #include "parsing.h"
 
-bool	nb_word_env(char *str, int *word, char c)
-{
-	int		i;
+// bool	nb_word_env(char *str, int *word, char c)
+// {
+// 	int		i;
 
-	i = 0;
-	while (str[i])
-	{
-		if ((str[i] != ' ' && (str[i] < '\t' || str[i] > '\r'))
-			&& (str[i + 1] == ' ' || (str[i + 1] >= '\t'
-					&& str[i + 1] <= '\r')))
-			*word += 1;
-		i++;
-	}
-	if ((str[i - 1] == ' ' || (str[i - 1] >= '\t' && str[i - 1] <= '\r'))
-		&& c == '\0')
-        return (true);
-    return (false);
-}
+// 	i = 0;
+// 	while (str[i])
+// 	{
+// 		if ((str[i] != ' ' && (str[i] < '\t' || str[i] > '\r'))
+// 			&& (str[i + 1] == ' ' || (str[i + 1] >= '\t'
+// 					&& str[i + 1] <= '\r')))
+// 			*word += 1;
+// 		i++;
+// 	}
+// 	if ((str[i - 1] == ' ' || (str[i - 1] >= '\t' && str[i - 1] <= '\r'))
+// 		&& c == '\0')
+//         return (true);
+//     return (false);
+// }
 
-void	in_quotes(char *data, t_exp *var)
-{
-	char	quotetype;
+// void	in_quotes(char *data, t_exp *var)
+// {
+// 	char	quotetype;
 
-	quotetype = data[var->j];
-	var->j += 1;
-	while (data[var->j] && data[var->j] != quotetype)
-		var->j += 1;
-}
+// 	quotetype = data[var->j];
+// 	var->j += 1;
+// 	while (data[var->j] && data[var->j] != quotetype)
+// 		var->j += 1;
+// }
 
-void	check_var_env(char *data, t_env **env, t_exp *var)
-{
-	int	len;
+// void	check_var_env(char *data, t_env **env, t_exp *var)
+// {
+// 	int	len;
 
-	while (!var->str && data[var->j] && data[var->j] == '$')
-	{
-		var->str = search_env(data + (var->j + 1), env);
-		if (!var->str)
-		{
-			var->j += 1;
-			var->null += 1;
-			while (data[var->j] && data[var->j] != '$' && data[var->j] != '"'
-				&& data[var->j] != '\'')
-			{
-				var->null += 1;
-				var->j += 1;
-			}
-		}
-	}
-	if (var->null == (int)ft_strlen(data))
-		var->dollar = true;
-	var->null++;
-	len = (int)ft_strlen(var->tmp) - 1;
-	if (!var->str && data[var->j] == '\0' && (var->j - var->null) > 0
-		&& data[var->j - var->null] < 0 && var->tmp && (var->tmp[len] == ' '
-		|| (var->tmp[len] >= '\t' && var->tmp[len] <= '\r')))
-		var->dollar = true;
-}
+// 	while (!var->str && data[var->j] && data[var->j] == '$')
+// 	{
+// 		var->str = search_env(data + (var->j + 1), env);
+// 		if (!var->str)
+// 		{
+// 			var->j += 1;
+// 			var->null += 1;
+// 			while (data[var->j] && data[var->j] != '$' && data[var->j] != '"'
+// 				&& data[var->j] != '\'')
+// 			{
+// 				var->null += 1;
+// 				var->j += 1;
+// 			}
+// 		}
+// 	}
+// 	if (var->null == (int)ft_strlen(data))
+// 		var->dollar = true;
+// 	var->null++;
+// 	len = (int)ft_strlen(var->tmp) - 1;
+// 	if (!var->str && data[var->j] == '\0' && (var->j - var->null) > 0
+// 		&& data[var->j - var->null] < 0 && var->tmp && (var->tmp[len] == ' '
+// 		|| (var->tmp[len] >= '\t' && var->tmp[len] <= '\r')))
+// 		var->dollar = true;
+// }
 
-void	var_env(char *data, int *word, t_exp *var)
-{
-	int	len;
+// void	var_env(char *data, int *word, t_exp *var)
+// {
+// 	int	len;
 
-	len = 0;
-	if (var->j != 0 && (var->j - var->null) > 0 && data[var->j - var->null] < 0)
-	{
-		len = (int)ft_strlen(var->tmp) - 1;
-		if (var->tmp && var->tmp[len] != ' ' && (var->tmp[len] < '\t' || var->tmp[len] > '\r')
-			&& (var->str && (var->str[0] == ' ' || (var->str[0] >= '\t' && var->str[0] <= '\r'))))
-		{
-			*word += 1;
-			// var->save[var->i][var->j] *= -1;
-		}
-	}
-	if (var->j != 0 && (var->j - var->null) > 0 && data[var->j - var->null] > 0
-		&& ((var->str && (var->str[0] == ' ' || (var->str[0] >= '\t' && var->str[0] <= '\r')))))
-		*word += 1;
-	var->j += 1;
-	while (data[var->j] && data[var->j] != '$' && data[var->j] != '"'
-		&& data[var->j] != '\'')
-	{
-		data[var->j] *= -1;
-		var->j += 1;
-	}
-}
+// 	len = 0;
+// 	if (var->j != 0 && (var->j - var->null) > 0 && data[var->j - var->null] < 0)
+// 	{
+// 		len = (int)ft_strlen(var->tmp) - 1;
+// 		if (var->tmp && var->tmp[len] != ' ' && (var->tmp[len] < '\t' || var->tmp[len] > '\r')
+// 			&& (var->str && (var->str[0] == ' ' || (var->str[0] >= '\t' && var->str[0] <= '\r'))))
+// 		{
+// 			*word += 1;
+// 			// var->save[var->i][var->j] *= -1;
+// 		}
+// 	}
+// 	if (var->j != 0 && (var->j - var->null) > 0 && data[var->j - var->null] > 0
+// 		&& ((var->str && (var->str[0] == ' ' || (var->str[0] >= '\t' && var->str[0] <= '\r')))))
+// 		*word += 1;
+// 	var->j += 1;
+// 	while (data[var->j] && data[var->j] != '$' && data[var->j] != '"'
+// 		&& data[var->j] != '\'')
+// 	{
+// 		data[var->j] *= -1;
+// 		var->j += 1;
+// 	}
+// }
 
-void	in_dollar(char *data, t_env **env, int *word, t_exp *var)
-{
-	var->null = 0;
-	check_var_env(data, env, var);
-	if (var->str)
-	{
-		var_env(data, word, var);
-		var->dollar = nb_word_env(var->str, word, data[var->j]);
-		var->tmp = var->str;
-		var->str = NULL;
-	}
-}
+// void	in_dollar(char *data, t_env **env, int *word, t_exp *var)
+// {
+// 	var->null = 0;
+// 	check_var_env(data, env, var);
+// 	if (var->str)
+// 	{
+// 		var_env(data, word, var);
+// 		var->dollar = nb_word_env(var->str, word, data[var->j]);
+// 		var->tmp = var->str;
+// 		var->str = NULL;
+// 	}
+// }
 
-void	nb_word(char **data, char **save, t_env **env, int *word)
-{
-	t_exp	var;
+// void	nb_word(char **data, char **save, t_env **env, int *word)
+// {
+// 	t_exp	var;
 
-	var.i = 0;
-	var.null = 0;
-	var.str = NULL;
-	var.tmp = NULL;
-	var.save = save;
-	while (data[var.i])
-	{
-		var.dollar = false;
-		var.j = 0;
-		while (data[var.i][var.j])
-		{
-			if (data[var.i][var.j] == '"' || data[var.i][var.j] == '\'')
-				in_quotes(data[var.i], &var);
-			if (data[var.i][var.j] == '$')
-				in_dollar(data[var.i], env, word, &var);
-			else if (data[var.i][var.j])
-				var.j++;
-		}
-		if (var.dollar == false)
-            *word += 1;
-		var.i++;
-	}
-	free_dtab(data);
-	// revert(data);
-}
+// 	var.i = 0;
+// 	var.null = 0;
+// 	var.str = NULL;
+// 	var.tmp = NULL;
+// 	var.save = save;
+// 	while (data[var.i])
+// 	{
+// 		var.dollar = false;
+// 		var.j = 0;
+// 		while (data[var.i][var.j])
+// 		{
+// 			if (data[var.i][var.j] == '"' || data[var.i][var.j] == '\'')
+// 				in_quotes(data[var.i], &var);
+// 			if (data[var.i][var.j] == '$')
+// 				in_dollar(data[var.i], env, word, &var);
+// 			else if (data[var.i][var.j])
+// 				var.j++;
+// 		}
+// 		if (var.dollar == false)
+//             *word += 1;
+// 		var.i++;
+// 	}
+// 	free_dtab(data);
+// 	// revert(data);
+// }
