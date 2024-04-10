15a9325fef20f2487df1e05eb7c1f4fb3e248765
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-04-10 17:57:59 +0200

    norm && fixed condition if expand returns null

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,35 +6,46 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 17:58:56 by csweetin          #+#    #+#             */
-/*   Updated: 2024/04/09 16:38:02 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/04/10 17:51:21 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-void	parsing(t_input **input, t_env **env, char *line)
+int	search_for_expand(t_input **input, t_env **env)
 {
 	t_input	*node;
 
 	node = NULL;
+	node = *input;
+	while (node)
+	{
+		if (node->tok != heredoc)
+		{
+			if (check_for_dollar(node))
+			{
+				node->data = expand(node->data, env);
+				if (!node->data)
+				{
+					input_freelst(input);
+					return (1);
+				}
+			}
+		}
+		// else
+			// check if delimiter is in quotes (single or double) or not
+		node = node->next;
+	}
+	return (0);
+}
+
+void	parsing(t_input **input, t_env **env, char *line)
+{
 	if (tokenization(input, env, line))
 		return ;
-	if (input && *input)
-	{
-		node = *input;
-		while (node)
-		{
-			if (node->tok != heredoc)
-				if (check_for_dollar(node))
-					if (expand(node, env))
-						fatal_exit(input, errno, NULL);
-			// else
-				// check if delimiter is in quotes (single or double) or not
-			node = node->next;
-		}
-	}
+	if (search_for_expand(input, env))
+		return ;
 	//remove_quote()
 	//find_builtin()
-	if (cmd_path(input, env))
-		return ;
+	cmd_path(input, env);
 }
