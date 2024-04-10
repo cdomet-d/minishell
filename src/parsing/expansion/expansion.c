acee84b93edb20b1c57d408e85ef5e56811a2365
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-04-10 17:58:25 +0200

    changed data type of return value

--- src/parsing/expansion/expansion.c
+++ src/parsing/expansion/expansion.c
@@ -6,13 +6,13 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 18:28:27 by csweetin          #+#    #+#             */
-/*   Updated: 2024/04/04 19:47:07 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/04/10 16:03:56 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-int	expand(t_input *node, t_env **env)
+char	**expand(char **data, t_env **env)
 {
 	char	**newtab;
 	char	**temp;
@@ -21,17 +21,17 @@ int	expand(t_input *node, t_env **env)
 	newtab = NULL;
 	temp = NULL;
 	word = 0;
-	temp = nb_word(node->data, env, &word);
+	temp = nb_word(data, env, &word);
 	if (!temp)
-		return (1);
+		return (NULL);
 	newtab = ft_calloc(sizeof(char *), word + 1);
 	if (!newtab)
-		return (1);
+		return (NULL);
 	newtab = nb_letter(temp, newtab);
 	if (!newtab)
-		return (1);
-	free_dtab(node->data);
-	node->data = newtab;
+		return (NULL);
+	free_dtab(data);
+	data = newtab;
 	free_dtab(temp);
-	return (0);
+	return (data);
 }
