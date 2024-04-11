08d53f52759ac98b922f02c2ecfd15a544cf3705
cha202 <116841786+cha202@users.noreply.github.com>
2024-04-11 15:22:45 +0200

    added condition for $?

--- src/parsing/expansion/replace_var_env.c
+++ src/parsing/expansion/replace_var_env.c
@@ -18,7 +18,7 @@ void	ft_copy_env(char *data, char *newtab, t_env **env, int *j)
 	}
 }
 
-void	ft_copy(char *data, char *newtab, t_env **env)
+void	ft_copy(char *data, char *newtab, t_env **env, int rv)
 {
 	int	i;
 	int	j;
@@ -30,18 +30,26 @@ void	ft_copy(char *data, char *newtab, t_env **env)
 		if (data[i] && data[i] == '$')
 		{
 			i++;
-			ft_copy_env(data + i, newtab, env, &j);
-			while (data[i] && data[i] != '$'  && data[i] > 0
-				&& data[i] != '"' && data[i] != ' '
-				&& (data[i] > '\t' || data[i] < '\r') && data[i] != '-')
+			if (data[i] == '?')
+			{
+				newtab[j++] = 48 + rv;
 				i++;
+			}
+			else if (data[i])
+			{
+				ft_copy_env(data + i, newtab, env, &j);
+				while (data[i] && data[i] != '$'  && data[i] > 0
+					&& data[i] != '"' && data[i] != ' '
+					&& (data[i] > '\t' || data[i] < '\r') && data[i] != '-')
+					i++;
+			}
 		}
-		if (data[i] < 0)
+		else if (data[i] < 0)
 		{
 			while (data[i] && data[i] < 0)
 				newtab[j++] = data[i++];
-        }
-		else if (data[i] && data[i] != '$')
+		}
+		else if (data[i])
 			newtab[j++] = data[i++];
 	}
 }
@@ -62,29 +70,29 @@ void	count_in_env(char *str, int *letter)
 
 void    put_in_neg(char *data)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (data[i])
-    {
+	i = 0;
+	while (data[i])
+	{
 		if (data[i] == ' ' || (data[i] >= '\t' && data[i] <= '\r'))
 			data[i] *= -1;
-        if (data[i] == '"')
-        {
-            i++;
-            while (data[i] && data[i] != '"')
-                i++;
-        }
-        if (data[i] == '\'')
-        {
-            data[i++] *= -1;
-            while (data[i] && data[i] != '\'')
-                data[i++] *= -1;
-            data[i++] *= -1;
-        }
-        else if (data[i])
-            i++;
-    }
+		if (data[i] == '"')
+		{
+			i++;
+			while (data[i] && data[i] != '"')
+				i++;
+		}
+		if (data[i] == '\'')
+		{
+			data[i++] *= -1;
+			while (data[i] && data[i] != '\'')
+				data[i++] *= -1;
+			data[i++] *= -1;
+		}
+		else if (data[i])
+			i++;
+	}
 }
 
 int	letters(char *data, t_env **env)
@@ -94,24 +102,35 @@ int	letters(char *data, t_env **env)
 
 	letter = 0;
 	j = 0;
-    put_in_neg(data);
+	put_in_neg(data);
 	while (data[j])
 	{
 		if (data[j] == '$')
 		{
 			j++;
-			count_in_env(search_env(data + j, env), &letter);
-			while (data[j] && data[j] != '$' && data[j] > 0
-				&& data[j] != '"' && data[j] != ' '
-				&& (data[j] > '\t' || data[j] < '\r') && data[j] != '-')
+			if (data[j] && data[j] == '?')
+			{
 				j++;
+				letter++;
+			}
+			else if (data[j])
+			{
+				count_in_env(search_env(data + j, env), &letter);
+				while (data[j] && data[j] != '$' && data[j] > 0
+					&& data[j] != '"' && data[j] != ' '
+					&& (data[j] > '\t' || data[j] < '\r') && data[j] != '-')
+					j++;
+			}		
 		}
-		if (data[j] && data[j] < 0)
-        {
-            while (data[j] && data[j] < 0)
-                j++;
-        }
-		else if (data[j] && data[j] != '$')
+		else if (data[j] && data[j] < 0)
+		{
+			while (data[j] && data[j] < 0)
+			{
+				j++;
+				letter++;
+			}
+		}
+		else if (data[j])
 		{
 			j++;
 			letter++;
