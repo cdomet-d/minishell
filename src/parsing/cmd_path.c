9d6ab22e80aef37b2234f3e340675f2d1f6e2f5c
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-04-15 18:52:46 +0200

    now if cmd doesn't exist cmd stays equal to its initial value

--- src/parsing/cmd_path.c
+++ src/parsing/cmd_path.c
@@ -6,7 +6,7 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 17:25:53 by csweetin          #+#    #+#             */
-/*   Updated: 2024/04/10 17:54:41 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/04/15 18:47:41 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,13 +45,12 @@ char	*find_path(char *cmd, char *env)
 		fill_word(path + letter, cmd, (int)ft_strlen(cmd));
 		if (access(path, X_OK) == 0)
 			return (path);
-		else if (env[i] == '\0')
-			return (path);
 		free(path);
 		path = NULL;
-		i++;
+		if (env[i])
+			i++;
 	}
-	return (NULL);
+	return (cmd);
 }
 
 int	path_slash(char *cmd)
@@ -68,13 +67,26 @@ int	path_slash(char *cmd)
 	return (0);
 }
 
+int	put_path(t_input *input, t_env	*node)
+{
+	char	*path;
+
+	path = NULL;
+	path = find_path(input->data[0], node->env + 5);
+	if (!path)
+		return (1);
+	if (!ft_strncmp(path, input->data[0], ft_strlen(input->data[0]) + 1))
+		return (0);
+	free(input->data[0]);
+	input->data[0] = path;
+	return (0);
+}
+
 int	cmd_path(t_input *input, t_env **env)
 {
 	t_env	*node;
-	char	*path;
 
 	node = *env;
-	path = NULL;
 	if (path_slash(input->data[0]))
 		return (0);
 	while (node)
@@ -82,14 +94,7 @@ int	cmd_path(t_input *input, t_env **env)
 		if (!ft_strncmp(node->env, "PATH=", 5))
 		{
 			if (node->env[5])
-			{
-				path = find_path(input->data[0], node->env + 5);
-				if (!path)
-					return (1);
-				free(input->data[0]);
-				input->data[0] = path;
-				return (0);
-			}
+				return (put_path(input, node));
 		}
 		node = node->next;
 	}
