5bdbb9ba33717f159934008616c58549a94d6efc
cha202 <116841786+cha202@users.noreply.github.com>
2024-04-15 09:42:19 +0200

    moved 2 functions from file expand_split_utils  to file expand_split

--- src/parsing/expansion/expand_split_utils.c
+++ src/parsing/expansion/expand_split_utils.c
@@ -12,48 +12,6 @@
 
 #include "parsing.h"
 
-void	fill_word(char *new, char *old, int i)
-{
-	int	j;
-
-	j = 0;
-	while (old[j] && i > 0)
-	{
-		new[j] = old[j];
-		j++;
-		i--;
-	}
-}
-
-int	nb_letter_str(char *data, int *j, int letter)
-{
-	char	quotetype;
-
-	while (data[*j] && (data[*j] == ' '
-			|| (data[*j] >= '\t' && data[*j] <= '\r')))
-		*j += 1;
-	while (data[*j] && data[*j] != ' ' && (data[*j] < '\t' || data[*j] > '\r'))
-	{
-		if (data[*j] == '"' || data[*j] == '\'')
-		{
-			quotetype = data[*j];
-			*j += 1;
-			letter += 1;
-			while (data[*j] && data[*j] != quotetype)
-			{
-				letter += 1;
-				*j += 1;
-			}
-		}
-		if (data[*j])
-		{
-			letter += 1;
-			*j += 1;
-		}
-	}
-	return (letter);
-}
-
 void	put_in_neg(char *data)
 {
 	int	i;
