fe41596eef4166e1bcf3e8a792981b3c105080b6
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-04-16 15:28:32 +0200

    env done

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -8,7 +8,13 @@ int	env(t_input *node)
 	{
 		if (ft_putstr_fd("minishell : env : too many arguments\n", 2) == -1)
 			return (1);
-		return (0); //????
+		return (0);
+	}
+	while (node->env)
+	{
+		if (ft_putstr_fd(node->env->env, 1) == -1)
+			return (1);
+		node->env = node->env->next;
 	}
 	return (0);
 }
