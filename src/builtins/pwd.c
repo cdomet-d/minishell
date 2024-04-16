520856f096976227c32cb19f8ccfdb02e8a90fb7
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-04-16 19:20:32 +0200

    pwd kinda done

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -1,3 +1,25 @@
 //HEADER
 
 #include "exec.h"
+
+int	pwd(char **data)
+{
+	char	*str;
+
+	str = NULL;
+	if (data[1])
+	{
+		if (ft_putstr_fd("pwd : too many arguments\n", 2) == -1)
+			return (1);
+		return (0);
+	}
+	str = getcwd(NULL, 0);
+	if (!str)
+		return (1);
+	if (ft_putstr_fd(str, 1) == -1)
+		return (1);
+	if (write(1, "\n", 1) == -1)
+		return (1);
+	free(str);
+	return (0);
+}
