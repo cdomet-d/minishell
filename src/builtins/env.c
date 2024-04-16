1b7eb59fe67b120d4952663afdc2b02d7e8d8175
cha202 <116841786+cha202@users.noreply.github.com>
2024-04-16 14:10:14 +0200

    started env

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -1,2 +1,13 @@
 //HEADER
 
+#include "exec.h"
+
+int env(t_input *node)
+{
+    if (node->data[1])
+    {
+        if (ft_putstr_fd("too many arguments\n", 2) == -1)
+            return (1);
+        return (0); //????
+    }
+}
