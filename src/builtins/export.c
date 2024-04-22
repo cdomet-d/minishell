43d9fa9b7a4f7854fdc982cc168403b19420e0d8
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-04-22 19:06:52 +0200

    started parsing export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 09:23:04 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/03/29 15:08:45 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/04/22 19:03:09 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,32 @@ static void	*exprt_inenv(t_env **env, char *data)
 	return (new);
 }
 
+int	check_arg(char *var)
+{
+	int	i;
+
+	i = 0;
+	if (var[0] != '_' || !ft_isalpha(var[0]))
+	{
+		print_error(errno, "minishell: export: '");
+		print_error(errno, var);
+		print_error(errno, "': not a valid identifier");
+		return (1);
+	}
+	while (var[i])
+	{
+		if (var[i] != '_' || !ft_isalnum(var[i]))
+		{
+			print_error(errno, "minishell: export: '");
+			print_error(errno, var);
+			print_error(errno, "': not a valid identifier");
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 void	*export(t_env *env, char *var)
 {
 	char	*key;
@@ -30,6 +56,8 @@ void	*export(t_env *env, char *var)
 
 	if (!env || !var)
 		return (print_error(errno, "NULL param in export"));
+	if (check_arg(var))
+		return ;
 	key = split_wsep(var, '=');
 	if (!key)
 		return (print_error(errno, NULL));
