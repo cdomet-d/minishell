c6df50829a86ff561b578422c7497b82f906472e
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-04-24 17:07:50 +0200

    modif put_in_neg

--- src/parsing/expansion/expand_split_utils.c
+++ src/parsing/expansion/expand_split_utils.c
@@ -6,7 +6,7 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 16:08:33 by csweetin          #+#    #+#             */
-/*   Updated: 2024/04/23 19:35:34 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/04/24 17:06:37 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,11 @@ void	put_in_neg(char *data, char quote1, char quote2)
 		{
 			i++;
 			while (data[i] && data[i] != quote2)
-				data[i++] *= -1;
+			{
+				if (data[i] > 0)
+					data[i] *= -1;
+				i++;
+			}
 		}
 		if (data[i])
 			i++;
@@ -49,9 +53,6 @@ int	check_ws(char **tab)
 			return (1);
 		while (tab[i][j])
 		{
-			if (tab[i][j] == '$'
-				&& (tab[i][j + 1] == '"' || tab[i][j + 1] == '\''))
-				return (1);
 			if (tab[i][j] == '"')
 				while (tab[i][j] && tab[i][j] != '"')
 					j++;
