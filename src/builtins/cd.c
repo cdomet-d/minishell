a87c8d1bb1cc513372407c08dfd58613666f9cdf
cha202 <116841786+cha202@users.noreply.github.com>
2024-04-26 13:38:07 +0200

    changed error message if HOME isn't found

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -4,26 +4,25 @@
 
 char    *find_home(t_env *env)
 {
-    while (env)
-    {
-        if (!ft_strncmp(env->env, "HOME=", 5))
-            return (env->env + 5);
-        env = env->next;
-    }
-    return (NULL);
+	while (env)
+	{
+		if (!ft_strncmp(env->env, "HOME=", 5))
+			return (env->env + 5);
+		env = env->next;
+	}
+	return (NULL);
 }
 
 int cd(t_input *in)
 {
-    char    *home;
+	char    *home;
 
-    if (!in->data[1])
-    {
-        home = find_home(in->env);
-        if (!home)
-            ft_putendl_fd("missing HOME variable", STDERR_FILENO);
-        else
-            chdir(home);
-    }
-    return (0);
+	if (!in->data[1])
+	{
+		home = find_home(in->env);
+		if (!home)
+			return (ft_putendl_fd("minishell : cd : HOME not set", STDERR_FILENO));
+		chdir(home);
+	}
+	return (0);
 }
