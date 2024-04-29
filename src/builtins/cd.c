2265c4433678122afba38f479416f7cebbc48df6
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-04-29 19:13:47 +0200

    cd doesn't work but getting there

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -40,19 +40,17 @@ int	change_pwd(t_env **env, char *path, char *var)
 	return (0);
 }
 
-char	*find_directory(t_env *env, char *var)
+char	*check_directory(char *var)
 {
 	char		*path;
 	struct stat	buf;
 
-	(void)env;
 	path = ft_strjoin("./", var);
 	if (!path)
 		return (print_error(errno, NULL), NULL);
 	stat(path, &buf);
 	// if (stat(path, &buf) == -1)
 	// {
-	// 	printf("in\n");
 	// 	free(path);
 	// 	path = NULL;
 	// 	return (print_error(errno, NULL), NULL);
@@ -70,6 +68,69 @@ char	*find_directory(t_env *env, char *var)
 	}
 	return (path);
 }
+char	*canonical_form(char *apath)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	temp = NULL;
+	while (apath[i])
+	{
+		j = 0;
+		if (apath[i] == '.')
+		{
+			temp = ft_substr(apath, 0, i);
+			if (!temp)
+				return (free(apath), NULL);
+			j++;
+			while (apath[i + j] && apath[i + j] == '/')
+				j++;
+			apath = ft_strjoin(temp, apath + (i + j));
+			if (!apath)
+				return (free(temp), NULL);
+		}
+		if (apath[i])
+			i++;
+	}
+	printf("%s\n", apath);
+	if (temp)
+		free(temp);
+	return (apath);
+}
+
+int	cd_path(t_input *in, char **path, char **apath)
+{
+	(void)apath;
+	if (!in->data[1])
+	{
+		*path = find_var_env(in->env, "HOME=");
+		if (!*path)
+			return (ft_putendl_fd("minishell: cd: HOME not set", 2), 1);
+	}
+	else if (in->data[1][0] == '-' && !in->data[1][1])
+	{
+		*path = find_var_env(in->env, "OLDPWD=");
+		if (!*path)
+			return (ft_putendl_fd("minishell: cd: OLDPWD not set", 2), 1);
+	}
+	else if (in->data[1][0] == '/')
+	{
+		*apath = ft_strdup(in->data[1]);
+		*apath = canonical_form(*apath);
+		*path = *apath;
+	}
+	else
+	{
+		*apath = check_directory(in->data[1]);
+		if (!*apath)
+			return (1);
+		*apath = canonical_form(*apath);
+		*path = *apath;
+	}
+	return (0);
+}
 
 int	cd(t_input *in)
 {
@@ -86,32 +147,14 @@ int	cd(t_input *in)
 		len++;
 	if (len > 2)
 		return (ft_putendl_fd("minishell: cd: too many arguments", 2), 1);
-	if (!in->data[1])
-	{
-		path = find_var_env(in->env, "HOME=");
-		if (!path)
-			return (ft_putendl_fd("minishell: cd: HOME not set", 2), 1);
-	}
-	else if (in->data[1][0] == '-' && !in->data[1][1])
-	{
-		path = find_var_env(in->env, "OLDPWD=");
-		if (!path)
-			return (ft_putendl_fd("minishell: cd: OLDPWD not set", 2), 1);
-	}
-	else if (in->data[1][0] == '/')
-		path = in->data[1];
-	else
-	{
-		apath = find_directory(in->env, in->data[1]);
-		if (!apath)
-			return (1);
-		path = apath;
-	}
+	if (cd_path(in, &path, &apath))
+		return (1);
+	printf("path : %s\n", path);
 	if (chdir(path) == -1)
 	{
 		if (apath)
 			free(apath);
-		return (1);
+		return (print_error(errno, NULL), 1);
 	}
 	temp = ft_strdup(find_var_env(in->env, "PWD="));
 	if (!temp)
