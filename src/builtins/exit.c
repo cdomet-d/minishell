1227a8aa2b0bd01c1e49c11a51c8dfafd6819ead
Jauppp <coralie.dometdemont@gmail.com>
2024-05-07 11:11:55 +0200

    no major changes

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: jauseff <jauseff@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 11:54:27 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/05/06 17:42:07 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/05/07 11:01:43 by jauseff          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	mh_exit(char *line, t_input *in)
 	if (in->data[1])
 		fatal_exit(&in, ft_atoi(in->data[1]), "exit");
 	else
-		fatal_exit(&in, 0, "exit");
+		fatal_exit(&in, in->status, "exit");
 }
