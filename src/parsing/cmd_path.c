3c9d5a9f1849cbe538fce912f0fa4a4be514c284
Charlotte Sweeting <charlottesweeting@outlook.fr>
2024-05-16 16:58:08 +0200

    got old version back

--- src/parsing/cmd_path.c
+++ src/parsing/cmd_path.c
@@ -6,7 +6,7 @@
 /*   By: csweetin <csweetin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 17:25:53 by csweetin          #+#    #+#             */
-/*   Updated: 2024/05/16 16:17:57 by csweetin         ###   ########.fr       */
+/*   Updated: 2024/05/16 16:57:06 by csweetin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,21 @@ static char	*find_path(char *cmd, char *env)
 	return (cmd);
 }
 
-static int	put_path(t_input *input, t_env	*node, int *status)
+static int	path_slash(char *cmd)
+{
+	size_t	i;
+
+	i = 0;
+	while (cmd[i])
+	{
+		if (cmd[i] == '/')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	put_path(t_input *input, t_env	*node)
 {
 	char	*path;
 
@@ -50,88 +64,29 @@ static int	put_path(t_input *input, t_env	*node, int *status)
 	if (!path)
 		return (1);
 	if (!ft_strncmp(path, input->data[0], ft_strlen(input->data[0]) + 1))
-	{
-		ft_putstr_fd(input->data[0], 2);
-		ft_putendl_fd(": command not found", 2);
-		*status = 127;
-		free_dtab(input->data);
-		input->data = NULL;
 		return (0);
-	}
 	free(input->data[0]);
 	input->data[0] = path;
 	return (0);
 }
 
-static int	check_path_slash(t_input *input, int *status)
-{
-	struct stat	buf;
-	char		*cmd;
-
-	cmd = input->data[0];
-	if (access(cmd, X_OK) == 0)
-	{
-		if (stat(cmd, &buf) == -1)
-			return (print_error(errno, "minishell: parsing"), -1);
-		if (S_ISDIR(buf.st_mode))
-		{
-			parsing_error("minishell: ", cmd, ": Is a directory");
-			*status = 126;
-			free_dtab(input->data);
-			input->data = NULL;
-		}
-	}
-	else
-	{
-		parsing_error("minishell: ", cmd, ": No such file or directory");
-		*status = 127;
-		free_dtab(input->data);
-		input->data = NULL;
-	}
-	return (1);
-}
-
-static int	path_slash(t_input *input, int *status)
-{
-	char	*cmd;
-	size_t	i;
-
-	cmd = input->data[0];
-	i = 0;
-	while (cmd[i])
-	{
-		if (cmd[i] == '/')
-			return (check_path_slash(input, status));
-		i++;
-	}
-	return (0);
-}
-
-int	cmd_path(t_input *input, t_env **env, int *status)
+int	cmd_path(t_input *input, t_env **env)
 {
 	t_env	*node;
-	int		rv;
 
 	node = *env;
 	if (!input->data || !input->data[0][0])
 		return (0);
-	rv = path_slash(input, status);
-	if (rv == 1)
+	if (path_slash(input->data[0]))
 		return (0);
-	if (rv == -1)
-		return (1);
 	while (node)
 	{
 		if (!ft_strncmp(node->env, "PATH=", 5))
 		{
 			if (node->env[5])
-				return (put_path(input, node, status));
+				return (put_path(input, node));
 		}
 		node = node->next;
 	}
-	parsing_error("minishell: ", input->data[0], ": No such file or directory");
-	*status = 127;
-	free_dtab(input->data);
-	input->data = NULL;
 	return (0);
 }
