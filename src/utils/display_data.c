d8e3ab7965d46eaa48f463a3a610afe4a9bbe632
cdomet-d <coralie.dometdemont@gmail.com>
2024-05-22 12:10:27 +0200

    merged all redirections in a single loop so they are created and redirected in the correct order ; PR for testing purposes

--- src/utils/display_data.c
+++ src/utils/display_data.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   display_data.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jauseff <jauseff@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 11:49:56 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/05/08 18:18:04 by jauseff          ###   ########lyon.fr   */
+/*   Updated: 2024/05/22 11:40:47 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	print_fds(t_fd *fd)
 {
-	fprintf(stderr, "%.20s\npfd[R] - %d || pfd[W] - %d || tmpin = %d || fd.pnb = %ld\n", \
-	"-- fds --------------------------", fd->pfd[R],
-		fd->pfd[W], fd->tmpin, fd->pnb);
+	fprintf(stderr, "%.20s\n pfd[R] - %d || pfd[W] - %d || ffd - %d || tmpin = %d || fd.pnb = %ld\n", "-- fds --------------------------",
+	fd->pfd[R], fd->pfd[W], fd->ffd, fd->tmpin, fd->pnb);
 }
